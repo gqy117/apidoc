1e32a0c17dd54f3ffd5e927b8a4994d5bc81ae0d
Leo Li <Leo.Li@myob.com>
2016-09-29 13:57:48 +1300

    Changed reference package name.

--- test/custom_markdown_parser_test.js
+++ test/custom_markdown_parser_test.js
@@ -5,7 +5,7 @@
  */
 
 // node modules
-var apidoc = require('apidoc-core');
+var apidoc = require('apidoc-core-myob');
 var exec   = require('child_process').exec;
 var fs     = require('fs-extra');
 var path   = require('path');
